110e7bf89fbf0c17f8235d5c55a6ff76c44bcf5c
Alexander <alex_de@gmx.de>
2017-08-14 03:24:50 +0200

    Add build task

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,35 @@
-const gulp = require('gulp');
-const gulp_tslint = require('gulp-tslint');
+const gulp = require("gulp");
+const clean = require("gulp-clean");
+const sourcemaps = require("gulp-sourcemaps");
+const tslint = require("gulp-tslint");
+const ts = require("gulp-typescript");
+const uglify = require('gulp-uglify');
 
-gulp.task('tslint', () => {
-    gulp.src(['src/*.ts'])
-      .pipe(gulp_tslint({
-          formatter: "prose"
-      }))
-      .pipe(gulp_tslint.report({
-          emitError: false
-      }));
+var tsProject = ts.createProject("tsconfig.json");
+var distPath = tsProject.config.compilerOptions.outDir;
+
+gulp.task("tslint", () => {
+    tsProject.src()
+        .pipe(tslint({
+            formatter: "prose"
+        }))
+        .pipe(tslint.report({
+            emitError: false
+        }));
+});
+
+gulp.task("clean", () => {
+    return gulp.src(distPath, {
+        read: false
+    }).pipe(clean());
+});
+
+gulp.task("build", ["tslint", "clean"], () => {
+    return tsProject.src()
+        .pipe(sourcemaps.init())
+        .pipe(tsProject())
+        .js
+        .pipe(uglify())
+        .pipe(sourcemaps.write("."))
+        .pipe(gulp.dest(distPath));
 });
--- src/VirtualDOM.ts
+++ src/VirtualDOM.ts
@@ -1,1 +1,7 @@
 import VirtualNode from "./VirtualNode";
+
+export default class VirtualDOM {
+    constructor() {
+        return;
+    }
+}
